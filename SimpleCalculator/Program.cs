e3f7ee220ef8d200937af0a24be0a7221831c8e8
Stevan Sapun <stevan.sapun@gmail.com>
2023-03-10 14:34:10 +0100

    added rest of the program.

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,10 +1,35 @@
-﻿internal class Program
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("broj operacija broj");
-        string s = Console.ReadLine();
+        string[] s = Console.ReadLine().Split(" ");
 
+        int a = Convert.ToInt32(s[0]), b = Convert.ToInt32(s[2]), r = 0;
+        string operacija = s[1];
+
+        switch (operacija)
+        {
+            case "+":
+                r = a + b;       
+                break;
+            case "-":
+                r = a - b;
+                break;
+            case "*":
+                r = a * b;
+                break;
+            case "/":
+                r = a / b;
+                break;
+            default:
+                r = 0;
+                break;
+        }
+
+        Console.WriteLine("rezultat = " + r.ToString());
     }
 
 }
