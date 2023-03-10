92bedf9e8bec219a99bf7e91b0350eff6096b2b5
Stevan Sapun <stevan.sapun@gmail.com>
2023-03-10 14:08:06 +0100

    Initial commit.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("broj operacija broj");
+        string s = Console.ReadLine();
+
+    }
+
+}
